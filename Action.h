2f83f7bbdda63881a7411beded6fcf6e68b6ebd0
tome09 <ciel.ro.ohs2019@gmail.com>
2022-07-13 12:41:47 +0900

    基底クラス作成

---
+++ Action.h
@@ -0,0 +1,14 @@
+/// 2022/07/13
+/// yoshitome
+/// Action.h
+
+class Action {
+    private:
+
+    public:
+        Action();
+        virtual ~Action();
+
+        virtual int8_t run(int32_t speed) = 0;
+
+}
